c8295e578f23919eacaa69652f9fc836af6053c9
Katsiaryna Ramaniuk <Katsiaryna_Ramaniuk@epplwarw02ba.home>
2023-06-15 17:26:12 +0200

    feat: implement fn that creates new file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,30 @@
+/** @format */
+
+import fs, { access, constants } from "node:fs/promises";
+
+const checkFileExists = async (newFilePath, errorMessage) => {
+	try {
+		await access(newFilePath, constants.F_OK);
+		throw new Error(errorMessage);
+	} catch (err) {
+		if (err.code !== "ENOENT") {
+			console.error(err);
+		}
+	}
+};
+
 const create = async () => {
-    // Write your code here 
+	const newFilePath = "src/fs/files/fresh.txt";
+	const newFileContent = "I am fresh and young";
+	const errorMessage = "FS operation failed";
+
+	checkFileExists(newFilePath, errorMessage);
+
+	try {
+		await fs.writeFile(newFilePath, newFileContent);
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 await create();
