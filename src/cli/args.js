406ff159c0cffe812102eaa90121f12d7224307e
KatiaR <hometsa@gmail.com>
2023-06-18 23:05:06 +0200

    feat: add cli fns-env and args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    const cliArgs = process.argv.slice(2);
+		cliArgs.forEach((arg, ind) => {
+			if (arg.startsWith("--")) {
+				const prop = cliArgs[ind].substring(2);
+				const value = cliArgs[ind + 1];
+				console.log(`${prop} is ${value}`);
+			}
+		});
+    
 };
 
 parseArgs();
